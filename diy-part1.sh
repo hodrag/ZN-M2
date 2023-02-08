c3ec5fb274bf872d55e98fd053ffea884cae0238
hodrag <31184354+hodrag@users.noreply.github.com>
2023-02-08 01:43:01 -0600

    Create diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,3 +5,6 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf
 #主题列表
 #echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
 
+#istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
