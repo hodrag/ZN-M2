3893812acc135f4001ba32fc284c26fdfe46f47e
hodrag <31184354+hodrag@users.noreply.github.com>
2023-02-08 02:11:28 -0600

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,3 +10,18 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+src-git packages https://github.com/xiaoqingfengATGH/feeds-packages
+src-git luci https://github.com/xiaoqingfengATGH/feeds-luci
+src-git routing https://github.com/coolsnowwolf/routing
+src-git telephony https://git.openwrt.org/feed/telephony.git
+src-git pwluci https://github.com/homelede/openwrt-passwall.git;luci
+src-git pw https://github.com/homelede/openwrt-passwall.git;packages
+src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng
+src-git lienol https://github.com/homelede/openwrt-package.git;main
+# luci-theme-infinityfreedom
+src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
+# homeclash
+src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git
+# strongswanInDocker
+src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git
