5de15425e849892fbcab99f6d57de9c73f3cd308
hodrag <31184354+hodrag@users.noreply.github.com>
2023-02-18 21:03:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,19 +11,17 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-src-git packages https://github.com/xiaoqingfengATGH/feeds-packages
-src-git luci https://github.com/xiaoqingfengATGH/feeds-luci
-src-git routing https://github.com/coolsnowwolf/routing
-src-git telephony https://git.openwrt.org/feed/telephony.git
-src-git pwluci https://github.com/homelede/openwrt-passwall.git;luci
-src-git pw https://github.com/homelede/openwrt-passwall.git;packages
-src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng
-src-git lienol https://github.com/homelede/openwrt-package.git;main
+echo 'src-git packages https://github.com/xiaoqingfengATGH/feeds-packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/xiaoqingfengATGH/feeds-luci' >>feeds.conf.default
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+echo 'src-git pwluci https://github.com/homelede/openwrt-passwall.git;luci' >>feeds.conf.default
+echo 'src-git pw https://github.com/homelede/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' >>feeds.conf.default
+echo 'src-git lienol https://github.com/homelede/openwrt-package.git;main' >>feeds.conf.default
 # luci-theme-infinityfreedom
-src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
-# homeclash
-src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 # strongswanInDocker
-src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git
-src-git alist https://github.com/alist-org/alist.git
-src-git dockerman https://github.com/bixuehujin/dockerman.git
+echo 'src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' >>feeds.conf.default
+echo 'src-git alist https://github.com/alist-org/alist.git' >>feeds.conf.default
+echo 'src-git dockerman https://github.com/bixuehujin/dockerman.git' >>feeds.conf.default
